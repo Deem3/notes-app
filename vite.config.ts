1106bda12320c5b911ec6839e1cb54b68ac0f55b
Deem3 <erdemee.t@gmail.com>
2024-01-01 02:49:48 +0800

    fix: add secure: false option to API proxy

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
       '/api': {
         target: env.API_URL,
         changeOrigin: true,
+        secure: false,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
